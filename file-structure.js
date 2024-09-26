8125bb38d1b29821bba9437bd63e76313863890d
sailesh-turing <sailesh.r@turing.com>
2024-09-26 12:39:51 +0530

    Update index page to display the file structure

---
+++ file-structure.js
@@ -0,0 +1,17 @@
+let folders = [
+    "29590",
+    "29677",
+    "37286",
+    "37297",
+    "periodic-table-29755",
+    "matrix-multiplication-29699",
+    "add-in-bst-29803"
+];
+
+folders = folders.reverse();
+const fileStructure = folders.map(folderName => {
+    return {
+        folder: folderName,
+        files: ['modelA.html', 'mnodelB.html', 'idealResponse.html']
+    };
+});
